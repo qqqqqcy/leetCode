1cda319966422ef32990dac29e8f9ca6a293b257
xiajunqcy@163.com <17325quincy>
2018-07-02 18:12:16 +0800

    add asn

--- run/main.js
+++ run/main.js
@@ -1,42 +1,22 @@
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {string} s
+ * @return {boolean}
  */
-var threeSum = function (nums) {
-    console.log(nums.length)
-    let map = {},
-    res = [],
-    repeat = {}
-    time = new Date()
-    function deal(m, n) {
-        for (let i = 0; i < m.length; i++) {
-            if (m[i][0] !== n && m[i][1] !== n) {
-                let newRes = [nums[n], nums[m[i][0]], nums[m[i][1]]].sort()
-                let key = newRes.join('_')
-                if (!repeat[key]) {
-                    repeat[key] = true
-                    res.push(newRes)
-                }
-            }
+var isValid = function (s) {
+    let i = 0
+    s = s.replace(new RegExp(' ', 'g'), '').split('')
+    while (i < s.length - 1) {
+        let sum = s[i] + s[i + 1]
+        while (sum === '()' || sum === '[]' || sum === '{}') {
+            s.splice(i, 2)
+            sum = s[i] + s[i + 1]
+        }
+        sum = s[i - 1] + s[i]
+        if (sum === '()' || sum === '[]' || sum === '{}') {
+            i--
+        } else {
+            i++
         }
     }
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            let num = nums[i] + nums[j]
-            if (map[num]) {
-                map[num].push([i, j])
-            } else {
-                map[num] = [
-                        [i, j]
-                ]
-            }
-        }
-    }
-    for (let i = 0; i < nums.length; i++) {
-        if (map[0 - nums[i]]) {
-            deal(map[0 - nums[i]], i)
-        }
-    }
-    console.log(new Date() - time)
-    return res
+    return (!s.length)
 };
