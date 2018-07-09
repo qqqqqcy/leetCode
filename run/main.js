a8fff8c6f834c4ef1a5e0c30557f92fdb4d81a2f
QQQQQCY <xiajunqcy@163.com>
2018-07-09 23:06:22 +0800

    add ans

--- run/main.js
+++ run/main.js
@@ -3,15 +3,17 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
+var isMatch = function (s, p) {
     let dp = new Array()
-    for (let i = 0; i <= s.length; i++) {
+    for (let i = 0; i < s.length; i++) {
         dp[i] = new Array()
     }
-    dp[0][0] = true
-    for (let i = 0; i <= s.length; i++) {
-        for (let j = 0; j <= p.length; j++) {
-            
-        }        
+    dp[-1][-1] = true
+    for (let i = -1; i < s.length; i++) {
+        for (let j = 0; j < p.length; j++) {
+            if (p[j] === '*') {
+                dp[i][j] = dp[i][j-1]
+            }
+        }
     }
 };
