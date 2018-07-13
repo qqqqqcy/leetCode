0e2a3c9b977aad81e36e0b0669dba34f6d78deb3
xiajunqcy@163.com <17325quincy>
2018-07-13 13:05:20 +0800

    add ans

--- run/main.js
+++ run/main.js
@@ -1,38 +1,27 @@
 /**
- * Definition for an interval.
- * function Interval(start, end) {
- *     this.start = start;
- *     this.end = end;
- * }
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
  */
-/**
- * @param {Interval[]} intervals
- * @param {Interval} newInterval
- * @return {Interval[]}
- */
-var insert = function (intervals, newInterval) {
-    let res = new Array(),
-        flag = true
-    for (let i = 0; i < intervals.length; i++) {
-        if (newInterval.start > intervals[i].end) {
-            res.push(intervals[i])
-        } else if (newInterval.end < intervals[i].start) {
-            if (flag) {
-                res.push(newInterval)
-                flag = false
-            }
-            res.push(intervals[i])
-        } else {
-            if (intervals[i].start < newInterval.start) {
-                newInterval.start = intervals[i].start
-            }
-            if (newInterval.end < intervals[i].end) {
-                newInterval.end = intervals[i].end
-            }
+var getPermutation = function (n, k) {
+    let str = '123456789',
+        res = new Array(),
+        sum = 0
 
+    function run(str) {
+        if (!str) {
+            sum++
+            return
+        }
+        for (let i = 0; i < str.length; i++) {
+            res.push(str[i])
+            run(str.replace(str[i], ''))
+            if (sum === k) {
+                return
+            }
+            res.pop()
         }
     }
-    if (flag) res.push(newInterval)
-    // return newInterval
-    return res
+    run(str.substr(0, n))
+    return res.join('')
 };
