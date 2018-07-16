751e9f271b3163b61a87f74ac15ea83599341ae6
xiajunqcy@163.com <17325quincy>
2018-07-16 18:34:52 +0800

    add ans

--- run/main.js
+++ run/main.js
@@ -1,22 +1,48 @@
 /**
- * @param {number} m
- * @param {number} n
- * @return {number}
+ * @param {string[]} words
+ * @param {number} maxWidth
+ * @return {string[]}
  */
-var uniquePaths = function (m, n) {
-    let dp = new Array()
-    dp[0] = new Array()
-    for (let i = 0; i < m; i++) {
-        dp[0][i] = 1
+var fullJustify = function (words, maxWidth) {
+    function getSpace(n) {
+        return new Array(n + 1).join(' ')
     }
-    for (let i = 1; i < n; i++) {
-        dp[i] = new Array()
-        for (let j = 0; j < m; j++) {
-            let tmp = 0
-            if (dp[i - 1]) tmp += dp[i - 1][j]
-            if (dp[i][j - 1]) tmp += dp[i][j - 1]
-            dp[i][j] = tmp
+    let res = new Array(),
+        tmp = [],
+        len = 0,
+        space = 0
+    tmp = [words[0]]
+    len = words[0].length
+    for (let i = 1; i < words.length; i++) {
+        if (len + words[i].length + 1 <= maxWidth) {
+            space++
+            len += words[i].length + 1
+            tmp.push(words[i])
+        } else {
+            let allSpace = space + maxWidth - len,
+                tmpStr = tmp[0]
+            if (tmp.length === 1) {
+                tmpStr += getSpace(maxWidth - tmp[0].length)
+            } else {
+                let gapSpace = getSpace(parseInt(allSpace / (tmp.length - 1))),
+                    leaveSpace = getSpace(allSpace % (tmp.length - 1))
+                for (let j = 1; j < tmp.length; j++) {
+                    tmpStr += gapSpace
+                    if (leaveSpace.length) {
+                        tmpStr += ' '
+                        leaveSpace = getSpace(leaveSpace.length - 1)
+                    }
+                    tmpStr += tmp[j]
+                }
+            }
+            res.push(tmpStr)
+            space = 0
+            tmp = [words[i]]
+            len = words[i].length
         }
     }
-    return dp[n - 1][m - 1]
+    tmpStr = tmp.join(' ')
+    tmpStr += getSpace(maxWidth - tmpStr.length)
+    res.push(tmpStr)
+    return res
 };
