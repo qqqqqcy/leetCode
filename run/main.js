a73c8e3d5be740211f5f49769c51640de77fc0f2
xiajunqcy@163.com <17325quincy>
2018-07-20 18:00:58 +0800

    add ans

--- run/main.js
+++ run/main.js
@@ -1,11 +1,34 @@
 /**
- * @param {number} n
- * @return {number}
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-var climbStairs = function(n) {
-    let dp = [1,1]
-    for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i-1] + dp[i-2]
+var sortColors = function (nums) {
+    let l = -1,
+        r = nums.length,
+        p = 0
+    while (p < r) {
+        if (nums[p] === 0) {
+            if (p - l === 1) {
+                l++
+                p++
+            } else {
+                l++
+                tmp = nums[l]
+                nums[l] = nums[p]
+                nums[p] = tmp
+            }
+        } else if (nums[p] === 2) {
+            if (r - l === 1) {
+                p++
+            } else {
+                r--
+                tmp = nums[r]
+                nums[r] = nums[p]
+                nums[p] = tmp
+            }
+        } else {
+            p++
+        }
     }
-    return dp[n]
+
 };
