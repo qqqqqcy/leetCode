187bada1987f591d88796d938e7ddc4578f707d0
QQQQQCY <xiajunqcy@163.com>
2018-09-27 01:03:30 +0800

    add handclap

--- run/main.js
+++ run/main.js
@@ -1,15 +1,40 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var removeDuplicates = function (nums) {
-    if (nums.length < 2) return nums
-    let t = 2
-    for (let h = 2; h < nums.length; h++) {
-        if (nums[h] !== nums[t - 2]) {
-            nums[t] = nums[h]
-            t++
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+    let i = new ListNode(),
+        res, j, after
+    i.next = head
+    while (i.next) {
+        if (i.next.val < x) {
+            if (!res) {
+                res = new ListNode()
+                res.next = i.next
+            }
+            i = i.next
+        } else {
+            if (!after) {
+                after = i.next
+                j = i.next
+
+            } else {
+                j.next = i.next
+                j = i.next
+            }
+            i.next = i.next.next
+            j.next = null
         }
     }
-    nums.length = t
+    i.next = after ? after : null
+    if (!res) res = i
+    return res.next
 };
