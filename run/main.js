aa049a703407d46b23374ceb68e1245c4a811ca0
Quincy <qqqqqcy@gmail.com>
2020-03-19 16:24:52 +0800

    121, 122, 123

--- run/main.js
+++ run/main.js
@@ -1,27 +1,81 @@
+// /**
+//  * @param {number} n
+//  * @param {number[][]} flights
+//  * @param {number} src
+//  * @param {number} dst
+//  * @param {number} K
+//  * @return {number}
+//  */
+// var findCheapestPrice = function(n, flights, src, dst, K) {
+//     // init map
+//     const fmap = new Array()     
+//     const checked = []
+//     for (let a = 0; a < n; a++) {
+//         checked[a] = a===src
+//         fmap[a] = []
+//         for (let b = 0; b < n; b++) {
+//             fmap[a][b] = Infinity
+//             if(a===b) {fmap[a][b] = 0}
+//         }
+//     }
+
+//     flights.map(([a,b,cost])=>{
+//         fmap[a][b] = cost
+//       })
+ 
+
+//     function findMid() {
+//         let cost = Infinity,mid
+//         fmap[src].map((val,index)=>{
+//             if (!checked[index] && val<cost) {
+//                 mid = index;
+//                 cost = val
+//             }
+//             checked[mid] = true
+//         })
+//         return mid;
+//     }
+
+//     while (checked.some(item=>!item)) {
+//         const mid = findMid();
+//         for (let p = 0; p < n; p++) {
+//             fmap[src][p] = Math.min(
+//                 fmap[src][p],
+//                 fmap[src][mid]+fmap[mid][p]
+//             )
+//         }
+//     }
+//     return fmap[src][dst]===Infinity?-1:fmap[src][dst]
+// };
+
+
+// n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+// src = 0, dst = 2, k = 0
+
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+ * @param {number[]} prices
+ * @return {number}
  */
-/**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
- */
-var buildTree = function(preorder, inorder) {
-    function run(preL,preR,inL,inR) {
-        if (preL>preR || preL<0 || preR>preorder.length ) return null
-        if (inL>inR || inL<0 || inR>inorder.length ) return null
-     
-        const curNode = new TreeNode(preorder[preL])
-        const inorderIndex = inorder.indexOf(preorder[preL])
-        if (preL===preR) return curNode
-        curNode.left = run(preL+1,preL+inorderIndex-inL,inL,inorderIndex-1)
-        curNode.right = run(preL+inorderIndex-inL+1,preR,inorderIndex+1,inR)
-        return curNode
+var maxProfit = function(prices =[]) {
+    let maxProfit = 0
+    for (let mid = 0; mid < prices.length; mid++) {
+        let preMyStock = prices[0], preMaxProfit = 0,afterMyStock = prices[mid], afterMaxProfit = 0
+        for (let i = 1; i < mid; i++) {
+            const currentStock = prices[i]
+            preMaxProfit = Math.max(preMaxProfit,currentStock-preMyStock)
+            preMyStock = Math.min(preMyStock,currentStock)
+        }
+        for (let i = mid; i < prices.length; i++) {
+            const currentStock = prices[i]
+            afterMaxProfit = Math.max(afterMaxProfit,currentStock-afterMyStock)
+            afterMyStock = Math.min(afterMyStock,currentStock)
+        }
+        maxProfit = Math.max(maxProfit,preMaxProfit+afterMaxProfit)
     }
-    if (!preorder) return null
-    return run(0,preorder.length-1,0,preorder.length-1)
+    return maxProfit
 };
+
+console.log(
+    maxProfit([3,3,5,0,0,3,1,4])
+);
+// Input: [7,1,5,3,6,4]
